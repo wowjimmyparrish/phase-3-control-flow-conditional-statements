cb4239cef7725458399542934bb5152b58102be0
James Parrish <jthomasparrishjr@gmail.com>
2022-11-06 19:10:23 -0600

    completed lab

--- control_flow.rb
+++ control_flow.rb
@@ -1,16 +1,48 @@
+require "pry"
+
 def admin_login(username, password)
   # your code here
+  if (username == "admin" || username =="ADMIN") && password == "12345"
+    "Access granted"
+  else
+    "Access denied"
+  end
 end
 
 def hows_the_weather(temperature)
   # your code here
+  if temperature < 40 
+    "It's brisk out there!"
+  elsif temperature >= 40 && <= 65
+    "It's chilly out there!"
+  elsif temperature > 85
+    "It's too dang hot out there!"
+  else
+    "It's perfect out there"
+  end
 end
 
 def fizzbuzz(num)
   # your code here
+  if num % 3 == 0
+    "Fizz"
+  elsif num % 5 == 0
+    "Buzz"
+  elsif num % 3 == 0 && num % 5 == 0
+    "FizzBuzz"
+  else
+    num
+  end
 end
 
 def calculator(operation, num1, num2)
   # your code here
+when "+" then num1 + num2
+when "-" then num1 - num2
+when "*" then num1 * num2
+when "/" then num1 / num2
+else "Invalid operation!"
 end
+end
+
 
